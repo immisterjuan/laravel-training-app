d8ba6c9ed372c81e22ee5a7581021a68df292faa
Juan <jan_caser@cody.inc>
2025-11-19 13:55:11 +0800

    Updated

--- resources/views/tickets/create.blade.php
+++ resources/views/tickets/create.blade.php
@@ -1,8 +1,14 @@
 @extends('layouts.app')
 @section('title','New Ticket')
+@section('head')
+<style></style>
+@endsection
 @section('content')
-    <div>
-        <!-- Simplicity is an acquired taste. - Katharine Gerould -->
-        Hello
-    </div>
+<div class="shadow-sm rounded-md p-4 mb-4 w-full">
+    <a href="{{ route('tickets.create') }}" class="inline-block px-3 py-2 rounded-md text-sm text-white bg-blue-500 hover:bg-blue-800 focus:outline-none cursor-pointer">
+        Add Ticket
+    </a>
+</div>
+<div class="bg-white shadow-sm rounded-md overflow-x-auto">
+</div>
 @endsection
